48ae98aa3276bdc55eeba0bfe28940c06de1ffa0
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2021-11-19 17:04:37 +0300

    first attempt

--- DSP.Lab5.Api/ImageCorrelation.cs
+++ DSP.Lab5.Api/ImageCorrelation.cs
@@ -12,16 +12,43 @@ namespace DSP.Lab5.Api
     {
         public static Point GetCoincidencePoint(Bitmap fragment, Bitmap original)
         {
-            int maxY = 0;
-            int maxX = 0;
-
-            int fragmentPixelSize = Image.GetPixelFormatSize(fragment.PixelFormat) / 8;
-            int originalPixelSize = Image.GetPixelFormatSize(original.PixelFormat) / 8;
+            int pixelSize = Image.GetPixelFormatSize(fragment.PixelFormat) / 8;
 
             byte[] fragmentBytes = GetBytes(fragment);
+            byte[] originalBytes = GetBytes(original);
+
+            int deltaHeight = original.Height - fragment.Height;
+            int deltaWidth = original.Width - fragment.Width;
+
+            int[] sums = new int[deltaHeight * deltaWidth];
+
+            for (int i = 0; i < deltaHeight; i++)
+            {
+                for (int j = 0; j < deltaWidth * pixelSize; j += pixelSize)
+                {
+                    for (int s = 0; s < fragment.Height; s++)
+                    {
+                        for (int k = 0; k < fragment.Width; k += pixelSize)
+                        {
+                            int index = (i + s) + (j + k);
+                            sums[i * deltaHeight + j / pixelSize] += fragmentBytes[index] * originalBytes[index];
+                        }
+                    }
+                }
+            }
+            
+            int max = sums.Max();
+            int indexMax = 0;
+            for (int i = 0; i < sums.Length; i++)
+            {
+                if (sums[i] == max)
+                {
+                    indexMax = i;
+                }
+            }
 
 
-            return new Point(maxX, maxY);
+            return new Point(indexMax / deltaHeight, indexMax - (indexMax / deltaHeight));
         }
 
         public static byte[] GetBytes(Bitmap bitmap)
